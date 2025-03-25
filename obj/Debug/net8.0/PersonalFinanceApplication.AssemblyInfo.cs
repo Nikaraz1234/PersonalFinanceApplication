2933fd5d89fd8dd08eecd45d3f38d030f7771012
Nikaraz1234 <77344739+Nikaraz1234@users.noreply.github.com>
2025-03-25 20:51:10 +0400

    .

--- obj/Debug/net8.0/PersonalFinanceApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalFinanceApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab309e7b3248e84ba633f358c213f020e55daa6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d668f39a91d6b144d575e5bc3eb56f6095a5c5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
