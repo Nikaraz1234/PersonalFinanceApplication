66b2f9768f0de785253070455109272fff6cb505
Nikaraz1234 <77344739+Nikaraz1234@users.noreply.github.com>
2025-03-26 01:59:07 +0400

    .

--- Services/RecurringBillsService.cs
+++ Services/RecurringBillsService.cs
@@ -3,15 +3,6 @@ using System;
 
 namespace PersonalFinanceApplication.Services
 {
-    public class RecurringBillsService : IRecurringBillService
-    {
-        private readonly AppDbContext _context;
-
-        public RecurringBillsService(AppDbContext context)
-        {
-            _context = context;
-        }
-
-    }
+ 
 }
     
--- obj/Debug/net8.0/PersonalFinanceApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalFinanceApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff67c4aa7c57ac5226eb0f6480082bbaab7ec083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899991edb45e3c5a811b9f370106a56162a025ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinanceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
